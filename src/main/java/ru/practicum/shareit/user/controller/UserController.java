8788ac2be703ed5b30f1e4dcdad646a0911efdc7
Pavel Spectr <120334093+PavelSpectr@users.noreply.github.com>
2023-09-17 02:49:55 +0800

    add-controllers -> Добавлена реализация для User и Item

---
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -0,0 +1,52 @@
+package ru.practicum.shareit.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.service.UserService;
+
+import javax.validation.Valid;
+import java.util.Collection;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping(path = "/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto createUser(@Valid @RequestBody UserDto userDto) {
+        log.info("Запрос на добавление пользователя {}", userDto);
+        return userService.addUser(userDto);
+    }
+
+    @PatchMapping("/{userId}")
+    public UserDto updateUser(@PathVariable Long userId, @RequestBody UserDto userDto) {
+        log.info("Запрос на обновление пользователя с id = {}, новые данные: {}", userId, userDto);
+        return userService.updateUser(userDto, userId);
+    }
+
+    @GetMapping
+    public Collection<UserDto> getAllUsers() {
+        log.info("Запрос на получение списка пользователей");
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{userId}")
+    public UserDto getUserById(@PathVariable Long userId) {
+        log.info("Запрос на получение данных пользователя с  id = {}", userId);
+        return userService.getUserById(userId);
+    }
+
+    @DeleteMapping("/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUserById(@PathVariable Long userId) {
+        log.info("Запрос на удаления пользователя с id = {}", userId);
+        userService.deleteUserById(userId);
+    }
+}
---
+++ src/main/java/ru/practicum/shareit/user/storage/inMemory/InMemoryUserStorage.java
@@ -0,0 +1,83 @@
+package ru.practicum.shareit.user.storage.inMemory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.exception.users.UserBadRequestException;
+import ru.practicum.shareit.exception.users.UserInvalidDataException;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.storage.UserStorage;
+
+import java.util.*;
+
+@Component
+@Slf4j
+public class InMemoryUserStorage implements UserStorage {
+
+    private final Map<Long, User> usersMap = new HashMap<>();
+    private final Set<String> emailSet = new HashSet<>();
+    private Long id = 0L;
+
+    private Long getNextId() {
+        return ++id;
+    }
+
+    @Override
+    public User add(User user) {
+        validateUserEmail(user.getEmail());
+        Long userId = getNextId();
+        log.info("Добавлен пользователь. userId = {}, user = {}", userId, user);
+        user.setId(userId);
+        usersMap.put(userId, user);
+        emailSet.add(user.getEmail());
+        return user;
+    }
+
+    @Override
+    public User update(User user, Long userId) {
+        User userUpdate = usersMap.get(userId);
+        validateUserExists(userId);
+        if (!Objects.equals(user.getEmail(), userUpdate.getEmail())) {
+            validateUserEmail(user.getEmail());
+        }
+        if (user.getName() != null) {
+            userUpdate.setName(user.getName());
+        }
+        if (user.getEmail() != null) {
+            emailSet.remove(userUpdate.getEmail());
+            emailSet.add(user.getEmail());
+            userUpdate.setEmail(user.getEmail());
+        }
+        log.info("User id = {} обновлён", userId);
+        return userUpdate;
+    }
+
+    @Override
+    public Collection<User> getAll() {
+        return usersMap.values();
+    }
+
+    @Override
+    public void deleteById(Long userId) {
+        validateUserExists(userId);
+        emailSet.remove(usersMap.get(userId).getEmail());
+        usersMap.remove(userId);
+        log.info("user id = {} удалён", userId);
+    }
+
+    @Override
+    public Optional<User> getById(Long id) {
+        return Optional.ofNullable(usersMap.get(id));
+    }
+
+    private void validateUserEmail(String email) { //Исправил алгоритм
+        if (emailSet.contains(email)) {
+            throw new UserInvalidDataException("Ошибка обновления. Такая почта уже существует");
+        }
+    }
+
+    private void validateUserExists(Long id) {
+        if (!Objects.equals(id, usersMap.get(id).getId())) { //Поменял алгоритм
+            throw new UserBadRequestException("Ошибка обновления пользователя. Id пользователя не найден");
+        }
+    }
+}
